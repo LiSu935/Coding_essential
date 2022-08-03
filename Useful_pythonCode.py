61279e48f5bb45d31552cca0c697a3504374a5b6
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2022-08-03 15:07:08 -0500

    Update Useful_pythonCode.py

--- Useful_pythonCode.py
+++ Useful_pythonCode.py
@@ -24,3 +24,35 @@ np.sum(array1>0 , axis=1)
                     # if the column is not null, then set the value to be 'Yes')
 # super useful!
 merge_df['Is_TF'] = np.where(merge_df['TF_ID'].isnull(), 'No', 'Yes')
+
+                    
+# ============================================================================================ #
+# merge several tables into a excel file:
+# ============================================================================================ #
+                    
+import pandas as pd
+import os
+#import openpyxl
+import xlsxwriter
+
+input_dir = '/Users/lisu/Documents/DrDongXu/Guoquan_UTSA_immune/per_modality_clu_clean/with_clean/' \
+            'immunarch/'
+
+prefix = 'CD8_T'
+dic = {}
+sample_name_list = []
+for filename in os.listdir(input_dir):
+    if (filename.startswith(prefix)) & (filename.endswith("frequencyTable.txt")):
+        sample_name_list.append(filename)
+
+for samplename in sample_name_list:
+    df = pd.read_table(input_dir+samplename, header=0)
+    dic[samplename.split('_')[2]] = df
+
+writer = pd.ExcelWriter(input_dir+prefix+'_geneUsage_summary.xlsx', engine='xlsxwriter')
+
+for sheet, frame in dic.items(): # .use .items for python 3.X
+    frame.to_excel(writer, sheet_name=sheet)
+
+#critical last step
+writer.save()
